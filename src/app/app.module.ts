9cebb45f152100817943f49fd6dd71dd993f3d78
manutyagi <manutyagi4@gmail.com>
2018-08-08 14:11:44 +0530

    First push ticketed events front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material';
+import { MoviesComponent } from './movies/movies.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MovieCardComponent } from './movies/movie-card/movie-card.component';
+import { AppRoutingModule } from "./app.routing.module";
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    HomePageComponent,
+    MovieCardComponent,
+    RegistrationFormComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
